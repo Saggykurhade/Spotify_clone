c77b159c3076bc6ada63b27385b4afdd8346280e
Saggykurhade <138295823+Saggykurhade@users.noreply.github.com>
2024-02-01 20:30:56 +0530

    updates

--- website/src/components/Homepage.jsx
+++ website/src/components/Homepage.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import './homepage.css';
+import './logo.svg'
 
 const Homepage = () => {
   return (
-    <div>Homepage</div>
+    <div>
+        {/* <img src="logo.svg" alt="" /> */}
+        <div className='left border'></div>
+        <div className='right border'></div>
+    </div>
   )
 }
 
