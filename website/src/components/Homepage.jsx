0aa30a0f8268477f8b65584122155440ebe1d86c
Saggykurhade <138295823+Saggykurhade@users.noreply.github.com>
2024-02-02 20:37:51 +0530

    updates

--- website/src/components/Homepage.jsx
+++ website/src/components/Homepage.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import './homepage.css';
+import './utility.css';
 import './logo.svg'
 
 const Homepage = () => {
   return (
-    <div>
-        {/* <img src="logo.svg" alt="" /> */}
-        <div className='left border'></div>
-        <div className='right border'></div>
+    <div className='container flex bg-black'>
+      <div className='left'>
+        <div className="home bg-grey rounded m-1 p-1">
+          <div className='logo'><img className='invert' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTWP2HkuDVbWVFf-uvJbGxTNMz0TEhCcAGy1w&usqp=CAU" alt="logo" /></div>
+          <ul>
+            <li className='icons'><i class="fa-solid fa-house"></i>Home</li>
+            <li className='icons'><i class="fa-solid fa-magnifying-glass"></i>Search</li>
+          </ul>
+        </div>
+        <div className="library bg-grey rounded m-1 p-1"> your library</div>
+      </div>
+      <div className='right border'></div>
     </div>
   )
 }
